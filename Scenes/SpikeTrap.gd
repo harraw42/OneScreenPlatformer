3bc268b0b2a99b157dec20428f148727eef7ba5e
harraw42 <harraw42@kingshigh.schoool.nz>
2022-07-28 12:24:13 +1200

    Swinging Traps and Spike Traps
    Added Swinging Trap and Spike Trap, both fully made and coded

--- Scenes/SpikeTrap.gd
+++ Scenes/SpikeTrap.gd
@@ -2,11 +2,9 @@ extends Node2D
 
 
 func _ready():
-	$AnimationPlayer.play("DoorClosed")
+	$AnimationPlayer.play("SpikeTrigger")
 
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
-		$AnimationPlayer.play("DoorOpen")
-		
-
+		print("Game Over")
--- SwingingTrap.gd
+++ SwingingTrap.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
-
 func _ready():
-	$AnimationPlayer.play("DoorClosed")
+	$AnimationPlayer.play("Swing")
+
 
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
-		$AnimationPlayer.play("DoorOpen")
-		
-
+		print("Game Over")
