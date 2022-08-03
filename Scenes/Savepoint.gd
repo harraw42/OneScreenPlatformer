acd2843b3a85df326e7e06e9d76bb334c8ef68ee
harraw42 <harraw42@kingshigh.schoool.nz>
2022-08-04 10:45:33 +1200

    Checkpoints
    + Props

--- Scenes/Savepoint.gd
+++ Scenes/Savepoint.gd
@@ -6,3 +6,10 @@ func _ready():
 func _process(delta):
 	if GameStats.get_spawn() != self:
 		$AnimationPlayer.play("Savepoint")
+
+
+
+func _on_SavePoint_body_entered(body):
+	if body.is_in_group("Player"):
+		GameStats.set_spawn(self)
+		$AnimationPlayer.play("Saving")
--- Scenes/World.gd
+++ Scenes/World.gd
@@ -5,6 +5,8 @@ func _ready():
 	pass 
 
 func _process(delta):
+	pass
 	if Input.is_action_just_pressed("reset"):
 		GameStats.reset()
-	$CanvasLayer/Control/Label.text = GameStats.get_time()
+	
+	#$CanvasLayer/Control/Label.text = GameStats.get_time()
