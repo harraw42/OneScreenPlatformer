114b0e4f6ea906b9ea3498db6bbfbc293a50bdbb
harraw42 <harraw42@kingshigh.schoool.nz>
2022-08-15 12:24:50 +1200

    Got reset working
    added a few more spike traps and extra area, got the reset system working, trap offset aswell

--- Scenes/SpikeTrap.gd
+++ Scenes/SpikeTrap.gd
@@ -1,11 +1,6 @@
 extends Node2D
 
+export (int) var offset  = 0
 
 func _ready():
-	$AnimationPlayer.play("SpikeTrigger")
-
-
-func _on_Area2D_body_entered(body):
-	if body.is_in_group("Player"):
-		if GameStats.check_reset() == false:
-			body.global_position = GameStats.get_spawn().global_position
+	$AnimationPlayer.seek(offset)
--- Scenes/World.gd
+++ Scenes/World.gd
@@ -1,11 +1,7 @@
 extends Node2D
 
-
 func _ready():
 	pass 
 
 func _process(delta):
-	if Input.is_action_just_pressed("reset"):
-		GameStats.reset()
-	
-	#$CanvasLayer/Control/Label.text = GameStats.get_time()
+	$CanvasLayer/Control/Label.text = GameStats.get_time()
