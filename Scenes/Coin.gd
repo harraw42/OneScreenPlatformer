48dfd148d5673d70ded63d38b28281c3ee8cb3c8
harraw42 <harraw42@kingshigh.schoool.nz>
2022-08-19 09:56:34 +1200

    Coins + Got Coin Counter Fully Working
    Added more to the bottom, new section close to finished product

--- Scenes/Coin.gd
+++ Scenes/Coin.gd
@@ -7,4 +7,8 @@ func _on_CoinArea_area_entered(area):
 	if area.is_in_group("Player"):
 		GameStats.coin_number+=1
 		queue_free()
-		
+
+func _process(delta):
+	pass
+	if Input.is_action_just_pressed("reset"):
+		GameStats.coin_number=0
